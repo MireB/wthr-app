272777249e10d4a61f11f96d719472d20cf99d9a
mirebosch <boschmire@gmail.com>
2020-11-30 13:16:26 +0100

    footer changed inside the div container

--- src/App.js
+++ src/App.js
@@ -6,18 +6,20 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <div className="container">Hello!</div>
-      <footer>
-        {" "}
-        Open source code by{" "}
-        <a
-          href="https://github.com/MireB/wthr-app"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Mire Bosch.
-        </a>
-      </footer>
+      <div className="container">
+        Hello!
+        <footer>
+          {" "}
+          Open source code by{" "}
+          <a
+            href="https://github.com/MireB/wthr-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Mire Bosch.
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
